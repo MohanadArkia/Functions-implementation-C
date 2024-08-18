1e1d26cc0a8e9e32ec6d57d613d095aebd3a1845
MohanadArkia <arkia.ar.47@gmail.com>
2024-08-18 12:49:10 +0300

    Implemented zFill()

--- Main.c
+++ Main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+void zFill (char* str, int width, char fill) {
+    int strLength = strlen(str); 
+    printf("%s", str);
+    for (int i = 0; i < width - strLength; i++) {
+        printf("%c", fill);
+    }
+    printf("\n");
+}
 
 void repeat (char* str, int repeats, char separator, bool latestSeparator) {
     for (int i = 0; i < repeats; i++) {
@@ -16,7 +26,11 @@ void repeat (char* str, int repeats, char separator, bool latestSeparator) {
 
 
 int main () {
-  repeat("Hello", 5, ';', true);
-  repeat("World", 5, ';', false);
+  /* repeat("Hello", 5, ';', true); */
+  /* repeat("World", 5, ';', false); */
+  zFill("C", 3, '+');
+  zFill("C", 2, '#');
+  zFill("C", 1, ' ');
+
   return 0;
 }
